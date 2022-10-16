222ac2f48f7ed4871bb85265fdcb77f50f95860b
max <max@maxhogan.dev>
2022-10-16 18:16:39 -0500

    Add light theme & bounding boxes

--- gen.js
+++ gen.js
@@ -21,8 +21,16 @@ const setContent = (username, handle, pfp, time, text) => {
     $("#tweet-text").innerText = text;
 };
 
+const setDecoration = (threeDots) => {
+    document.documentElement.style.setProperty("--three-dots-display", threeDots ? "block" : "none");
+};
+
 const COLOR_THEMES = {
-    LIGHT: 0,
+    LIGHT: {
+        "--background-color": "#FFF",
+        "--accent-color": "#536471",
+        "--text-color": "#000",
+    },
     DARK: {
         "--background-color": "#15202b",
         "--accent-color": "#8b98a5",
@@ -49,19 +57,64 @@ const setPadding = (width, paddingTop, paddingBottom, paddingLeft, paddingRight)
     document.documentElement.style.setProperty("--right-spacing", paddingRight + "px");
 };
 
+const timeOptions = {
+    SECONDS: 0,
+    MINUTES: 1,
+    HOURS: 2,
+    DAYS: 3,
+    DAYS_MONTHS: 4,
+    DAYS_MONTHS_YEARS: 5,
+};
+
+const monthsShort = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
 const randomize = () => {
     const username = randomAlphaNumericSpecialAllCase(randomRange(1, 30), true);
     const handle = "@" + randomAlphaNumericSpecialAllCase(randomRange(1, 15), false);
-    const pfp = "https://picsum.photos/200";
-    const time = randomRange(1, 24) + "h";
+    const pfp = `https://picsum.photos/seed/${randomAlphaNumericSpecialAllCase(10, false)}/48/48`;
     const text = randomAlphaNumericSpecialAllCase(randomRange(1, 300), true, false).trim();
+
+    const timeOption = timeOptions[Object.keys(timeOptions)[randomTo(Object.keys(timeOptions).length - 1)]];
+
+    let time;
+    switch (timeOption) {
+        case timeOptions.SECONDS:
+            time = randomRange(1, 60) + "s";
+            break;
+        case timeOptions.MINUTES:
+            time = randomRange(1, 60) + "m";
+            break;
+        case timeOptions.HOURS:
+            time = randomRange(1, 24) + "h";
+            break;
+        case timeOptions.DAYS:
+            time = randomRange(1, 30) + "d";
+            break;
+        case timeOptions.DAYS_MONTHS:
+            time = monthsShort[randomTo(11)] + " " + randomRange(1, 30);
+            break;
+        case timeOptions.DAYS_MONTHS_YEARS:
+            time = monthsShort[randomTo(11)] + " " + randomRange(1, 30) + ", " + randomRange(2000, 2020);
+            break;
+
+    }
 
     setContent(username, handle, pfp, time, text);
 
-    setPadding(randomRange(300, 600), randomRange(0, 50), randomRange(0, 50), randomRange(0, 50), randomRange(0, 50));
+    setPadding(randomRange(450, 600), randomRange(0, 50), randomRange(0, 50), randomRange(0, 50), randomRange(0, 50));
 
-    setColorTheme(COLOR_THEMES.DARK);
+    setColorTheme(COLOR_THEMES[Object.keys(COLOR_THEMES)[randomTo(Object.keys(COLOR_THEMES).length - 1)]]);
+
+    setDecoration(randomTo(1) === 1);
 };
 
 
-// randomize();
+randomize();
+
+
+const getBoundingBoxes = () => {
+    const pfp = $("#tweet-pfp").getBoundingClientRect();
+    const names = $("#tweet-names").getBoundingClientRect();
+
+    return {pfp, names};
+}
